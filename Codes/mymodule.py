d2ab97f28844452e5bf1fcd8b1f93fb50dca8652
Zynp-Altnl <96389274+Zynp-Altnl@users.noreply.github.com>
2021-12-20 14:36:52 +0300

    Add files via upload
    B substracting

--- Codes/mymodule.py
+++ Codes/mymodule.py
@@ -1,16 +1,26 @@
-"""def greeting(name):
-    print("hello"+ name)
-person1= {"name":"beyza","age":"19","country":"turkey"}"""
+# def greeting(name):
+#     print("Hello" + name)
+
+# person1 = {
+
+#     "name": "Zeynep",
+#     "age" : "20",
+#     "country": "İstanbul"
+# }
+# print(person1["age"])
+
+import S3new as a
+
+if a.matrix_a.length_matrix()==a.matrix_b.length_matrix():
+    for i in range(len(a.matrix_a.matrix)):
+        for j in range(len(a.matrix_a.matrix)):
+                a.matrix_a.matrix[i][j] = a.matrix_a.matrix[i][j] - a.matrix_b.matrix[i][j]
+    for i in range(len(a.matrix_b.matrix)):
+        print (a.matrix_a.matrix[i]) 
 
 
 
-import S3new as s
 
-if s.matrix_a.length_matrix()==25 and s.matrix_b.length_matrix()==25:
 
-    for i in range(len(s.matrix_a.matrix)):
-        
-           for j in range(len(s.matrix_a.matrix)):
-            s.matrix_a.matrix[i][j]= s.matrix_a.matrix[i][j] + s.matrix_b.matrix[i][j]
-    for i in range(len(s.matrix_b.matrix)):
-        print(s.matrix_a.matrix[i])
+                     
+
