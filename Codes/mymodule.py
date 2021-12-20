a0a17d3fb8b00455166a4dc942ff16dd8f751ce9
Beyza-N <77545899+Beyza-N@users.noreply.github.com>
2021-12-20 14:34:20 +0300

    Add files via upload
    B-adding matris_a,matris_b

---
+++ Codes/mymodule.py
@@ -0,0 +1,16 @@
+"""def greeting(name):
+    print("hello"+ name)
+person1= {"name":"beyza","age":"19","country":"turkey"}"""
+
+
+
+import S3new as s
+
+if s.matrix_a.length_matrix()==25 and s.matrix_b.length_matrix()==25:
+
+    for i in range(len(s.matrix_a.matrix)):
+        
+           for j in range(len(s.matrix_a.matrix)):
+            s.matrix_a.matrix[i][j]= s.matrix_a.matrix[i][j] + s.matrix_b.matrix[i][j]
+    for i in range(len(s.matrix_b.matrix)):
+        print(s.matrix_a.matrix[i])
