05246bcf15fbaca197a8e775a2fa34f7a12406f5
Michael B.Kh <michaelkhany@gmail.com>
2022-01-31 10:25:39 +0300

    Create readMe.m

---
+++ Codes/Samples/Sample2_S6/readMe.m
@@ -0,0 +1,7 @@
+Writing the modules based on teams:
+              Objectives                ...          ModuleName
+>> IO Data Normalization                ...     ovcm_norm
+>> Physics Calculations and Calibration ...     ovcm_physx
+>> Image Optimization                   ...     ovcm_optim
+>> Pattern Recognition                  ...     ovcm_recog
+>> Real-time Logging Section            ...     ovcm_log  
